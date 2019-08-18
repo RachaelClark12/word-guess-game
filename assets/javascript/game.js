2a765720b5d06e91d128e19a8ca8b62dc7f390a1
Rachael Clark <rachael.clark@uconn.edu>
2019-08-18 12:04:07 -0400

    css and js variables defined

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+
+//Variable Declaration//
+var words = ["salt", "match", "sad", "materialistic", "planes", "thread", "industry", "discovery", "tiny", "wicked", "banana"];
+var word = [Math.floor(Math.random() * words.length)];
+var letters = word.split("");
+var numOfLetters = letters.length;
+var answer = [];
+var blanks = numOfLetters.length
+
+
+//Console Log//
+console.log(words);
+console.log(word);
+console.log(letters);
+console.log(numOfLetters);
+console.log(answer);
+console.log(blanks);
+
+
+
+for (var i = 0; i < word.length; i++) {
+    answer[i].push("_");
+}
+
+while (reamaining > 0) {
+
+
+
+
+}
+
